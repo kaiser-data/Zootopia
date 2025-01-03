276efb74df2d196db89425c4b527787bae82f44b
kaiser-data <61822885+kaiser-data@users.noreply.github.com>
2025-01-03 12:59:28 +0100

    added user input

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -1,21 +1,33 @@
 import requests
-with open("API_KEY_NINJA.txt", "r") as f:
-    API_KEY = f.read()
 
-URL = "https://api.api-ninjas.com/v1/animals"
-PARAMS = {
-    "name": "fox"
-}
-headers = {
-    "X-Api-Key": API_KEY
-}
+def get_api_key():
+    with open("API_KEY_NINJA.txt", "r") as f:
+        API_KEY = f.read()
+    return API_KEY
 
-response = requests.get(URL, headers=headers, params=PARAMS)
+def get_animals(animal):
+    API_KEY = get_api_key()
+    URL = "https://api.api-ninjas.com/v1/animals"
+    PARAMS = {
+        "name": animal
+    }
+    headers = {
+        "X-Api-Key": API_KEY
+    }
 
-if response.status_code == 200:
-    data = response.json()
-    print(data)
-else:
-    print(f"Error: {response.status_code}, {response.text}")
+    response = requests.get(URL, headers=headers, params=PARAMS)
+
+    if response.status_code == 200:
+        data = response.json()
+        print(data)
+    else:
+        print(f"Error: {response.status_code}, {response.text}")
 
 
+def main():
+    animal = input("Enter a name of an animal: ")
+    get_animals(animal)
+    print("Website was successfully generated to the file animals.html.")
+
+if __name__ == "__main__":
+    main()
